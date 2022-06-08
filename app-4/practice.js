748fc0aaae1638e84409b5ec681ea4d690d8bb8e
ianblakesleet <ianblakesleet@gmail.com>
2022-06-08 18:44:25 -0400

    added path

--- app-4/practice.js
+++ app-4/practice.js
@@ -1,15 +1,31 @@
 const fs = require('fs')
-fs.appendFile('message.txt', 'Rando Message ESKETIT SKURRRT', err => {
-  if (err) console.log(err)
-  console.log('success in appending!')
-})
+const path = require('path')
 
-fs.readFile('./message.txt', 'utf8', (err, data) => {
+fs.appendFile(
+  path.join(__dirname, 'message.txt'),
+  'Rando Message ESKETIT SKURRRT',
+  err => {
+    if (err) console.log(err)
+    console.log('success in appending!')
+  }
+)
+
+fs.readFile(__dirname + '/message.txt', 'utf8', (err, data) => {
   if (err) console.log(err)
   console.log(data)
 })
 
-fs.writeFile('./message.txt', 'New Message', err => {
+fs.writeFile(__dirname + '/message.txt', 'New Message', err => {
   if (err) console.log(err)
   console.log('rewrote file!')
+})
+
+fs.rename(__dirname + '/message.txt', __dirname + '/coolMessage.txt', err => {
+  if (err) console.log(err)
+  console.log('renamed File!')
+})
+
+fs.unlink(__dirname + '/message.txt', err => {
+  console.log(err)
+  console.log('successfully deleted the message.txt file!')
 })
